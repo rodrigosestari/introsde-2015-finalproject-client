0c21af8899edfc08d4b73eb49b162cfdb05eb340
rodrigosestari <sestari@Rodrigos-MacBook-Air.local>
2015-12-25 15:23:42 +0100

    goal ws

--- src/systemlogic/processcentricservices/rest/client/GoalWS.java
+++ src/systemlogic/processcentricservices/rest/client/GoalWS.java
@@ -1,127 +1,80 @@
 package systemlogic.processcentricservices.rest.client;
 
-import java.io.StringReader;
+import java.io.File;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.glassfish.jersey.client.ClientConfig;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
-
+import systemlogic.businesslogicservices.dto.goal.Goal;
+import systemlogic.businesslogicservices.dto.goal.valutation.Goalview;
+import systemlogic.businesslogicservices.dto.goals.Goals;
+import util.JaxbUtil;
 
 public class GoalWS {
 
 	static ClientConfig clientConfig = new ClientConfig();
 	static Client client = ClientBuilder.newClient(clientConfig);
 
-	
-	
-	public static void getGoals(int idperson){
-	
+	public static Goals getGoals(int idperson) {
+
 		WebTarget service = client.target(userinterface.Client.getUri()).path("goal/" + idperson);
 		Response response = service.request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).get();
-	
-	}
-	
-	public static NodeList getNodes(String source, String query) throws Exception {
-		NodeList nl = null;
-		try {
-			if (!source.isEmpty()){
-				InputSource input_source = new InputSource(new StringReader(source));
+		String xml = response.readEntity(String.class);
 
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-				DocumentBuilder db = dbf.newDocumentBuilder();
-				org.w3c.dom.Document document = db.parse(input_source);
+		// get xsd
+		File xsdFile = new File("resource/Goals.xsd");
+		// um-marshal Xml into object people,
+		Goals goals = (Goals) JaxbUtil.xmlToJaxb("systemlogic.businesslogicservices.dto.goals", xml, xsdFile);
+		return goals;
 
-				XPathFactory xpathFactory = XPathFactory.newInstance();
-				XPath xpath = xpathFactory.newXPath();
-
-				nl = (NodeList) xpath.evaluate(query, document, XPathConstants.NODESET);
-			}
-		} catch (Exception e) {
-			nl =null;
-		}
-
-		return nl;
 	}
 
+	public static Goalview getGoalValitation(int idgoal) {
 
-
-
-	
-/*
-	public static void request1() throws IOException, JSONException, Exception {
-
-
-	
-
-		write("\n \n Request #1: GET ["+service.getUri().getPath()+"] Accept: APPLICATION_XML Content-type: APPLICATION_XML");
-
+		WebTarget service = client.target(userinterface.Client.getUri()).path("goalValuation/" + idgoal);
 		Response response = service.request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).get();
-		int httpStatus =response.getStatus();     		
 		String xml = response.readEntity(String.class);
 
-
-		NodeList n = getNodes(xml, "//person");
-		if (n.getLength() > 2) {
-			write("=> Result:OK"); 
-		} else {
-			write("=> Result:ERROR");
-		}
-
-		NodeList n1 = getNodes(xml, "//person[1]/idPerson/text()");
-		firstPerson = Integer.parseInt(n1.item(0).getNodeValue());
-		NodeList n2 = getNodes(xml, "//person[last()]/idPerson/text()");
-		lastPerson = Integer.parseInt(n2.item(0).getNodeValue());
-
-		write("=> HTTP Status: " +httpStatus);
-		write(xml);
+		// get xsd
+		File xsdFile = new File("resource/GoalsValutation.xsd");
+		// um-marshal Xml into object people,
+		Goalview goals = (Goalview) JaxbUtil.xmlToJaxb("systemlogic.businesslogicservices.dto.goal.valutation", xml,
+				xsdFile);
+		return goals;
 
 	}
 
+	public static boolean saveGoal(int idperson, int idmeasure, XMLGregorianCalendar start, XMLGregorianCalendar end,
+			String type, String signal, float value) {
 
-	public static void request3() throws Exception {
+		Goal goal = new Goal();
+		goal.setEnd(end);
+		goal.setSignal(signal);
+		goal.setType(type);
+		goal.setValue(value);
 
+		File xsdFile = new File("resource/Goal.xsd");
 
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-		WebTarget service = client.target(getBaseURI()).path("person/"+firstPerson);
+		String xml = JaxbUtil.jaxbToXml("systemlogic.businesslogicservices.dto.goal", goal, xsdFile);
+		WebTarget service = client.target(userinterface.Client.getUri()).path("goal/" + idperson + "/" + idmeasure);
 
-		write("\n \n Request #3: [PUT] ["+service.getUri().getPath()+"] Accept: APPLICATION_XML Content-type: APPLICATION_XML");
+		Response response = service.request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML)
+				.post(Entity.xml(xml));
+		xml = response.readEntity(String.class);
 
-	
-		NodeList nl = getNodes(xmlFistPerson, "//firstname/text()");
-		String name = nl.item(0).getNodeValue();
-		
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String newname = dateFormat.format(new Date());
-		xmlFistPerson = xmlFistPerson.replace(name,  "Changed_XML at "+newname);
-
-		write(xmlFistPerson);
-		Response response = service.request(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).put(Entity.xml(xmlFistPerson));
-		int httpStatus =response.getStatus(); 
-		String xml = response.readEntity(String.class);
-
-		if ((httpStatus == 201)){ //created
-			write("=> Result:OK");
-		}else{
-			write("=> Result:ERROR");
+		goal = (Goal) JaxbUtil.xmlToJaxb("systemlogic.businesslogicservices.dto.goal", xml, xsdFile);
+		if (null != goal) {
+			return true;
 		}
 
-		write("=> HTTP Status: " +httpStatus);
-		write(xml);
-
+		return false;
 	}
 
-*/
 }
